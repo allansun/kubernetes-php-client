e189d00de5f3a43415d5cf11a2e614e24dc64a5c
Allan Sun <allan.sun@bricre.com>
2017-05-31 00:24:06 +0100

    Remove unnecessary files from git

--- .idea/fileTemplates/includes/PHP File Header.php
+++
@@ -1,8 +0,0 @@
-/*
- * This file is part of Kubernete Client.
- *
- * (c) Allan Sun <allan.sun@bricre.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
