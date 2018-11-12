f0c94764c7ddfcad3af9ffe7dd98691ab5f1194a
Allan Sun <allan.sun@bricre.com>
2018-11-12 16:55:00 +0800

    Generated against Kubernetes version v1.12.1

--- dev_src/Command/RunCommand.php
+++ dev_src/Command/RunCommand.php
@@ -3,13 +3,16 @@
 namespace CodeGenerator\Command;
 
 
+use GitWrapper\Event\GitLoggerListener;
 use GitWrapper\GitWorkingCopy;
 use GitWrapper\GitWrapper;
 use GuzzleHttp\Client;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Logger\ConsoleLogger;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Filesystem\Filesystem;
 
@@ -19,22 +22,26 @@ class RunCommand extends Command
     protected $swaggerFile = APP_ROOT . '/openapi/swagger.json';
     /** @var GitWorkingCopy */
     protected $GitWorkingCopy;
-
+    /** @var LoggerInterface */
+    protected $logger;
 
     protected function configure()
     {
         $this->setName('run')
-            ->setDescription('Pull Swagger file from Kubernetes github')
+            ->setDescription('Pull Swagger file from Kubernetes github, generate new code and commit.')
             ->addArgument('version', InputArgument::REQUIRED, 'Kubernetes version number');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+
+        $this->logger = new ConsoleLogger($output);
         $version = $input->getArgument('version');
 
 //        $this->validateGitPrivateKey();
 
         $Git = new GitWrapper();
+        $Git->addLoggerListener(new GitLoggerListener($this->logger));
 //        $Git->setPrivateKey($this->githubDeployKeyFile);
         $this->GitWorkingCopy = $Git->workingCopy(APP_ROOT);
 
