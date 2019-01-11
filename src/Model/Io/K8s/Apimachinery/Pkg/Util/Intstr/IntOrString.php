c8e3f5defb65525212f082f65eb3370df4250ee8
Travis CI User <travis@example.org>
2019-01-11 11:12:22 +0000

    Generated against Kubernetes version v1.10.12

---
+++ src/Model/Io/K8s/Apimachinery/Pkg/Util/Intstr/IntOrString.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Kubernetes\Model\Io\K8s\Apimachinery\Pkg\Util\Intstr;
+
+class IntOrString extends \KubernetesRuntime\AbstractModel
+{
+
+    protected $isRawObject = true;
+
+
+}
+
