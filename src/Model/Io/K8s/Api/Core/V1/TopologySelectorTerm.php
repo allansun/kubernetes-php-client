9fad937526b6b14a830f439e5c0b0bfcbebfcd18
Travis CI User <travis@example.org>
2019-02-13 03:42:18 +0000

    Generated against Kubernetes version v1.14.0-alpha.3

---
+++ src/Model/Io/K8s/Api/Core/V1/TopologySelectorTerm.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Kubernetes\Model\Io\K8s\Api\Core\V1;
+
+/**
+ * A topology selector term represents the result of label queries. A null or empty
+ * topology selector term matches no objects. The requirements of them are ANDed.
+ * It provides a subset of functionality as NodeSelectorTerm. This is an alpha
+ * feature and may change in the future.
+ */
+class TopologySelectorTerm extends \KubernetesRuntime\AbstractModel
+{
+
+    /**
+     * A list of topology selector requirements by labels.
+     *
+     * @var TopologySelectorLabelRequirement[]
+     */
+    public $matchLabelExpressions = null;
+
+    protected $isRawObject = true;
+
+
+}
+
