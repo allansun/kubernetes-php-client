7c762f66a0065d97a5f9258282f871ff6a97d9bd
Travis CI User <travis@example.org>
2019-02-14 03:43:32 +0000

    Generated against Kubernetes version v1.10.14-beta.0

---
+++ src/Model/Io/K8s/Apimachinery/Pkg/Api/Resource/Quantity.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Kubernetes\Model\Io\K8s\Apimachinery\Pkg\Api\Resource;
+
+class Quantity extends \KubernetesRuntime\AbstractModel
+{
+
+    protected $isRawObject = true;
+
+
+}
+
