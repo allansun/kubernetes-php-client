fe94151567dd7200c00bbb538c3191c8c5cab10b
Travis CI User <travis@example.org>
2019-02-20 03:42:32 +0000

    Generated against Kubernetes version v1.15.0-alpha.0

---
+++ src/Model/Io/K8s/Apimachinery/Pkg/Apis/Meta/V1/Fields.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Kubernetes\Model\Io\K8s\Apimachinery\Pkg\Apis\Meta\V1;
+
+/**
+ * Fields stores a set of fields in a data structure like a Trie. To understand how
+ * this is used, see: https://github.com/kubernetes-sigs/structured-merge-diff
+ */
+class Fields extends \KubernetesRuntime\AbstractModel
+{
+
+    protected $isRawObject = true;
+
+
+}
+
