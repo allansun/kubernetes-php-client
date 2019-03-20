827c689facb98400177685f76ae5ca9f3022b5e9
Travis CI User <travis@example.org>
2019-03-20 03:42:27 +0000

    Generated against Kubernetes version v1.14.0-rc.1

---
+++ src/Model/Io/K8s/Api/Autoscaling/V2beta2/ExternalMetricStatus.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Kubernetes\Model\Io\K8s\Api\Autoscaling\V2beta2;
+
+/**
+ * ExternalMetricStatus indicates the current value of a global metric not
+ * associated with any Kubernetes object.
+ */
+class ExternalMetricStatus extends \KubernetesRuntime\AbstractModel
+{
+
+    /**
+     * current contains the current value for the given metric
+     *
+     * @var MetricValueStatus
+     */
+    public $current = null;
+
+    /**
+     * metric identifies the target metric by name and selector
+     *
+     * @var MetricIdentifier
+     */
+    public $metric = null;
+
+    protected $isRawObject = true;
+
+
+}
+
---
+++ src/Model/Io/K8s/Apiextensions_apiserver/Pkg/Apis/Apiextensions/V1beta1/CustomResourceSubresources.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Kubernetes\Model\Io\K8s\Apiextensions_apiserver\Pkg\Apis\Apiextensions\V1beta1;
+
+/**
+ * CustomResourceSubresources defines the status and scale subresources for
+ * CustomResources.
+ */
+class CustomResourceSubresources extends \KubernetesRuntime\AbstractModel
+{
+
+    /**
+     * Scale denotes the scale subresource for CustomResources
+     *
+     * @var CustomResourceSubresourceScale
+     */
+    public $scale = null;
+
+    /**
+     * Status denotes the status subresource for CustomResources
+     *
+     * @var CustomResourceSubresourceStatus
+     */
+    public $status = null;
+
+    protected $isRawObject = true;
+
+
+}
+
