03206535f7f960df927c7b50ff747ddd878c6f21
Travis CI User <travis@example.org>
2019-03-26 03:44:59 +0000

    Generated against Kubernetes version v1.14.1-beta.0

---
+++ src/Model/Io/K8s/Api/Auditregistration/V1alpha1/Webhook.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Kubernetes\Model\Io\K8s\Api\Auditregistration\V1alpha1;
+
+/**
+ * Webhook holds the configuration of the webhook
+ */
+class Webhook extends \KubernetesRuntime\AbstractModel
+{
+
+    /**
+     * ClientConfig holds the connection parameters for the webhook required
+     *
+     * @var WebhookClientConfig
+     */
+    public $clientConfig = null;
+
+    /**
+     * Throttle holds the options for throttling the webhook
+     *
+     * @var WebhookThrottleConfig
+     */
+    public $throttle = null;
+
+    protected $isRawObject = true;
+
+
+}
+
