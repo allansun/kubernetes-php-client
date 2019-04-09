6ed4f61a8dad93d48e1990094972941b3c7ec17f
Travis CI User <travis@example.org>
2019-04-09 03:42:10 +0000

    Generated against Kubernetes version v1.14.2-beta.0

---
+++ src/Model/Io/K8s/Api/Core/V1/ObjectFieldSelector.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Kubernetes\Model\Io\K8s\Api\Core\V1;
+
+/**
+ * ObjectFieldSelector selects an APIVersioned field of an object.
+ */
+class ObjectFieldSelector extends \KubernetesRuntime\AbstractModel
+{
+
+    /**
+     * Version of the schema the FieldPath is written in terms of, defaults to "v1".
+     *
+     * @var string
+     */
+    public $apiVersion = null;
+
+    /**
+     * Path of the field to select in the specified API version.
+     *
+     * @var string
+     */
+    public $fieldPath = null;
+
+    protected $isRawObject = true;
+
+
+}
+
---
+++ src/Model/Io/K8s/Api/Extensions/V1beta1/AllowedFlexVolume.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Kubernetes\Model\Io\K8s\Api\Extensions\V1beta1;
+
+/**
+ * AllowedFlexVolume represents a single Flexvolume that is allowed to be used.
+ * Deprecated: use AllowedFlexVolume from policy API Group instead.
+ */
+class AllowedFlexVolume extends \KubernetesRuntime\AbstractModel
+{
+
+    /**
+     * driver is the name of the Flexvolume driver.
+     *
+     * @var string
+     */
+    public $driver = null;
+
+    protected $isRawObject = true;
+
+
+}
+
