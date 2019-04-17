2b588ed69599d504c4bbd219ca7c689ac3709129
Travis CI User <travis@example.org>
2019-04-17 03:42:48 +0000

    Generated against Kubernetes version v1.15.0-alpha.1

---
+++ src/Model/Io/K8s/Api/Core/V1/DaemonEndpoint.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Kubernetes\Model\Io\K8s\Api\Core\V1;
+
+/**
+ * DaemonEndpoint contains information about a single Daemon endpoint.
+ */
+class DaemonEndpoint extends \KubernetesRuntime\AbstractModel
+{
+
+    /**
+     * Port number of the given endpoint.
+     *
+     * @var integer
+     */
+    public $Port = null;
+
+    protected $isRawObject = true;
+
+
+}
+
