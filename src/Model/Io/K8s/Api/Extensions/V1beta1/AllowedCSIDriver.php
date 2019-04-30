93116ce170fc8681bbc7b028b9d1cd557fada616
Travis CI User <travis@example.org>
2019-04-30 03:42:10 +0000

    Generated against Kubernetes version v1.15.0-alpha.2

---
+++ src/Model/Io/K8s/Api/Extensions/V1beta1/AllowedCSIDriver.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Kubernetes\Model\Io\K8s\Api\Extensions\V1beta1;
+
+/**
+ * AllowedCSIDriver represents a single inline CSI Driver that is allowed to be
+ * used.
+ */
+class AllowedCSIDriver extends \KubernetesRuntime\AbstractModel
+{
+
+    /**
+     * Name is the registered name of the CSI driver
+     *
+     * @var string
+     */
+    public $name = null;
+
+    protected $isRawObject = true;
+
+
+}
+
---
+++ src/Model/Io/K8s/Apiextensions_apiserver/Pkg/Apis/Apiextensions/V1beta1/ExternalDocumentation.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Kubernetes\Model\Io\K8s\Apiextensions_apiserver\Pkg\Apis\Apiextensions\V1beta1;
+
+/**
+ * ExternalDocumentation allows referencing an external resource for extended
+ * documentation.
+ */
+class ExternalDocumentation extends \KubernetesRuntime\AbstractModel
+{
+
+    /**
+     * @var string
+     */
+    public $description = null;
+
+    /**
+     * @var string
+     */
+    public $url = null;
+
+    protected $isRawObject = true;
+
+
+}
+
