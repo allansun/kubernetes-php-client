9f32f4829e6dbd5dc26f256478fc5c8875a6d764
Travis CI User <travis@example.org>
2019-05-08 03:42:10 +0000

    Generated against Kubernetes version v1.15.0-alpha.3

---
+++ src/Model/Io/K8s/Api/Core/V1/WindowsSecurityContextOptions.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Kubernetes\Model\Io\K8s\Api\Core\V1;
+
+/**
+ * WindowsSecurityContextOptions contain Windows-specific options and credentials.
+ */
+class WindowsSecurityContextOptions extends \KubernetesRuntime\AbstractModel
+{
+
+
+}
+
