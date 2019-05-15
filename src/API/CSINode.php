dd2890a779d0884a961a81c20344bd71b29028b8
Travis CI User <travis@example.org>
2019-05-15 03:42:10 +0000

    Generated against Kubernetes version v1.16.0-alpha.0

---
+++ src/API/CSINode.php
@@ -0,0 +1,258 @@
+<?php
+
+namespace Kubernetes\API;
+
+use \Kubernetes\Model\Io\K8s\Api\Storage\V1beta1\CSINodeList as CSINodeList;
+use \Kubernetes\Model\Io\K8s\Api\Storage\V1beta1\CSINode as TheCSINode;
+use \Kubernetes\Model\Io\K8s\Apimachinery\Pkg\Apis\Meta\V1\Status as Status;
+use \Kubernetes\Model\Io\K8s\Apimachinery\Pkg\Apis\Meta\V1\DeleteOptions as DeleteOptions;
+use \Kubernetes\Model\Io\K8s\Apimachinery\Pkg\Apis\Meta\V1\Patch as Patch;
+use \Kubernetes\Model\Io\K8s\Apimachinery\Pkg\Apis\Meta\V1\WatchEvent as WatchEvent;
+
+class CSINode extends \KubernetesRuntime\AbstractAPI
+{
+
+    /**
+     * list or watch objects of kind CSINode
+     *
+     * @configkey allowWatchBookmarks	boolean
+     * @configkey continue	string
+     * @configkey fieldSelector	string
+     * @configkey labelSelector	string
+     * @configkey limit	integer
+     * @configkey resourceVersion	string
+     * @configkey timeoutSeconds	integer
+     * @configkey watch	boolean
+     * @configkey allowWatchBookmarks	boolean
+     * @configkey continue	string
+     * @configkey fieldSelector	string
+     * @configkey labelSelector	string
+     * @configkey limit	integer
+     * @configkey resourceVersion	string
+     * @configkey timeoutSeconds	integer
+     * @configkey watch	boolean
+     * @param array $queries
+     * @return CSINodeList|mixed
+     */
+    public function list(array $queries = [])
+    {
+        return $this->parseResponse(
+        	$this->client->request('get',
+        		"/apis/storage.k8s.io/v1beta1/csinodes"
+        		,[
+        			'query' => $queries,
+        		]
+        	)
+        	, 'listStorageV1beta1CSINode'
+        );
+    }
+
+    /**
+     * create a CSINode
+     *
+     * @configkey dryRun	string
+     * @configkey fieldManager	string
+     * @configkey dryRun	string
+     * @configkey fieldManager	string
+     * @param TheCSINode $Model
+     * @param array $queries
+     * @return TheCSINode|mixed
+     */
+    public function create(\Kubernetes\Model\Io\K8s\Api\Storage\V1beta1\CSINode $Model, array $queries = [])
+    {
+        return $this->parseResponse(
+        	$this->client->request('post',
+        		"/apis/storage.k8s.io/v1beta1/csinodes"
+        		,[
+        			'json' => $Model->getArrayCopy(),
+        			'query' => $queries,
+        		]
+        	)
+        	, 'createStorageV1beta1CSINode'
+        );
+    }
+
+    /**
+     * delete collection of CSINode
+     *
+     * @configkey allowWatchBookmarks	boolean
+     * @configkey continue	string
+     * @configkey fieldSelector	string
+     * @configkey labelSelector	string
+     * @configkey limit	integer
+     * @configkey resourceVersion	string
+     * @configkey timeoutSeconds	integer
+     * @configkey watch	boolean
+     * @configkey allowWatchBookmarks	boolean
+     * @configkey continue	string
+     * @configkey fieldSelector	string
+     * @configkey labelSelector	string
+     * @configkey limit	integer
+     * @configkey resourceVersion	string
+     * @configkey timeoutSeconds	integer
+     * @configkey watch	boolean
+     * @param array $queries
+     * @return Status|mixed
+     */
+    public function deleteCollection(array $queries = [])
+    {
+        return $this->parseResponse(
+        	$this->client->request('delete',
+        		"/apis/storage.k8s.io/v1beta1/csinodes"
+        		,[
+        			'query' => $queries,
+        		]
+        	)
+        	, 'deleteStorageV1beta1CollectionCSINode'
+        );
+    }
+
+    /**
+     * read the specified CSINode
+     *
+     * @configkey exact	boolean
+     * @configkey export	boolean
+     * @configkey exact	boolean
+     * @configkey export	boolean
+     * @param $name
+     * @param array $queries
+     * @return TheCSINode|mixed
+     */
+    public function read($name, array $queries = [])
+    {
+        return $this->parseResponse(
+        	$this->client->request('get',
+        		"/apis/storage.k8s.io/v1beta1/csinodes/{$name}"
+        		,[
+        			'query' => $queries,
+        		]
+        	)
+        	, 'readStorageV1beta1CSINode'
+        );
+    }
+
+    /**
+     * replace the specified CSINode
+     *
+     * @configkey dryRun	string
+     * @configkey fieldManager	string
+     * @configkey dryRun	string
+     * @configkey fieldManager	string
+     * @param $name
+     * @param TheCSINode $Model
+     * @param array $queries
+     * @return TheCSINode|mixed
+     */
+    public function replace($name, \Kubernetes\Model\Io\K8s\Api\Storage\V1beta1\CSINode $Model, array $queries = [])
+    {
+        return $this->parseResponse(
+        	$this->client->request('put',
+        		"/apis/storage.k8s.io/v1beta1/csinodes/{$name}"
+        		,[
+        			'json' => $Model->getArrayCopy(),
+        			'query' => $queries,
+        		]
+        	)
+        	, 'replaceStorageV1beta1CSINode'
+        );
+    }
+
+    /**
+     * delete a CSINode
+     *
+     * @configkey dryRun	string
+     * @configkey gracePeriodSeconds	integer
+     * @configkey orphanDependents	boolean
+     * @configkey propagationPolicy	string
+     * @configkey dryRun	string
+     * @configkey gracePeriodSeconds	integer
+     * @configkey orphanDependents	boolean
+     * @configkey propagationPolicy	string
+     * @param $name
+     * @param DeleteOptions $Model
+     * @param array $queries
+     * @return Status|mixed
+     */
+    public function delete($name, \Kubernetes\Model\Io\K8s\Apimachinery\Pkg\Apis\Meta\V1\DeleteOptions $Model, array $queries = [])
+    {
+        return $this->parseResponse(
+        	$this->client->request('delete',
+        		"/apis/storage.k8s.io/v1beta1/csinodes/{$name}"
+        		,[
+        			'json' => $Model->getArrayCopy(),
+        			'query' => $queries,
+        		]
+        	)
+        	, 'deleteStorageV1beta1CSINode'
+        );
+    }
+
+    /**
+     * partially update the specified CSINode
+     *
+     * @configkey dryRun	string
+     * @configkey fieldManager	string
+     * @configkey force	boolean
+     * @configkey dryRun	string
+     * @configkey fieldManager	string
+     * @configkey force	boolean
+     * @param $name
+     * @param Patch $Model
+     * @param array $queries
+     * @return TheCSINode|mixed
+     */
+    public function patch($name, \Kubernetes\Model\Io\K8s\Apimachinery\Pkg\Apis\Meta\V1\Patch $Model, array $queries = [])
+    {
+        return $this->parseResponse(
+        	$this->client->request('patch',
+        		"/apis/storage.k8s.io/v1beta1/csinodes/{$name}"
+        		,[
+        			'json' => $Model->getArrayCopy(),
+        			'query' => $queries,
+        		]
+        	)
+        	, 'patchStorageV1beta1CSINode'
+        );
+    }
+
+    /**
+     * watch individual changes to a list of CSINode. deprecated: use the 'watch'
+     * parameter with a list operation instead.
+     *
+     * @return WatchEvent|mixed
+     */
+    public function watchList()
+    {
+        return $this->parseResponse(
+        	$this->client->request('get',
+        		"/apis/storage.k8s.io/v1beta1/watch/csinodes"
+        		,[
+        		]
+        	)
+        	, 'watchStorageV1beta1CSINodeList'
+        );
+    }
+
+    /**
+     * watch changes to an object of kind CSINode. deprecated: use the 'watch'
+     * parameter with a list operation instead, filtered to a single item with the
+     * 'fieldSelector' parameter.
+     *
+     * @param $name
+     * @return WatchEvent|mixed
+     */
+    public function watch($name)
+    {
+        return $this->parseResponse(
+        	$this->client->request('get',
+        		"/apis/storage.k8s.io/v1beta1/watch/csinodes/{$name}"
+        		,[
+        		]
+        	)
+        	, 'watchStorageV1beta1CSINode'
+        );
+    }
+
+
+}
+
