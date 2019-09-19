e45498a254c8901ddca8662eb9295e4082a5ec60
Travis CI User <travis@example.org>
2019-09-19 03:45:23 +0000

    Generated against Kubernetes version v1.15.5-beta.0

---
+++ src/Model/Io/K8s/Api/Core/V1/WindowsSecurityContextOptions.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Kubernetes\Model\Io\K8s\Api\Core\V1;
+
+/**
+ * WindowsSecurityContextOptions contain Windows-specific options and credentials.
+ */
+class WindowsSecurityContextOptions extends \KubernetesRuntime\AbstractModel
+{
+
+    /**
+     * GMSACredentialSpec is where the GMSA admission webhook
+     * (https://github.com/kubernetes-sigs/windows-gmsa) inlines the contents of the
+     * GMSA credential spec named by the GMSACredentialSpecName field. This field is
+     * alpha-level and is only honored by servers that enable the WindowsGMSA feature
+     * flag.
+     *
+     * @var string
+     */
+    public $gmsaCredentialSpec = null;
+
+    /**
+     * GMSACredentialSpecName is the name of the GMSA credential spec to use. This
+     * field is alpha-level and is only honored by servers that enable the WindowsGMSA
+     * feature flag.
+     *
+     * @var string
+     */
+    public $gmsaCredentialSpecName = null;
+
+
+}
+
