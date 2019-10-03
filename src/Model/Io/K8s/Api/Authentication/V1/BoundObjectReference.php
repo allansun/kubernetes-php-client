8e841d9cc5c134d79e7b8a9bdef90221e37a99a2
Travis CI User <travis@example.org>
2019-10-03 03:41:59 +0000

    Generated against Kubernetes version v1.16.1

---
+++ src/Model/Io/K8s/Api/Authentication/V1/BoundObjectReference.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Kubernetes\Model\Io\K8s\Api\Authentication\V1;
+
+/**
+ * BoundObjectReference is a reference to an object that a token is bound to.
+ */
+class BoundObjectReference extends \KubernetesRuntime\AbstractModel
+{
+
+    /**
+     * API version of the referent.
+     *
+     * @var string
+     */
+    public $apiVersion = null;
+
+    /**
+     * Kind of the referent. Valid kinds are 'Pod' and 'Secret'.
+     *
+     * @var string
+     */
+    public $kind = null;
+
+    /**
+     * Name of the referent.
+     *
+     * @var string
+     */
+    public $name = null;
+
+    /**
+     * UID of the referent.
+     *
+     * @var string
+     */
+    public $uid = null;
+
+
+}
+
