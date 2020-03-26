c6f841eb8a731331d59e882e4d98030b5cd4a3d2
Travis CI User <travis@example.org>
2020-03-26 03:42:45 +0000

    Generated against Kubernetes version v1.18.1-beta.0

---
+++ src/Model/Io/K8s/Api/Core/V1/NamespaceStatus.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Kubernetes\Model\Io\K8s\Api\Core\V1;
+
+/**
+ * NamespaceStatus is information about the current status of a Namespace.
+ */
+class NamespaceStatus extends \KubernetesRuntime\AbstractModel
+{
+
+    /**
+     * Represents the latest available observations of a namespace's current state.
+     *
+     * @var NamespaceCondition[]
+     */
+    public $conditions = null;
+
+    /**
+     * Phase is the current lifecycle phase of the namespace. More info:
+     * https://kubernetes.io/docs/tasks/administer-cluster/namespaces/
+     *
+     * @var string
+     */
+    public $phase = null;
+
+
+}
+
