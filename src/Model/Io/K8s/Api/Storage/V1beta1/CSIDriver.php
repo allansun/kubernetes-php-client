6ee87ec6a6815bc4ae347bb50457e194eeebedbb
Travis CI User <travis@example.org>
2020-05-07 03:41:49 +0000

    Generated against Kubernetes version v1.15.13-beta.0

---
+++ src/Model/Io/K8s/Api/Storage/V1beta1/CSIDriver.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Kubernetes\Model\Io\K8s\Api\Storage\V1beta1;
+
+/**
+ * CSIDriver captures information about a Container Storage Interface (CSI) volume
+ * driver deployed on the cluster. CSI drivers do not need to create the CSIDriver
+ * object directly. Instead they may use the cluster-driver-registrar sidecar
+ * container. When deployed with a CSI driver it automatically creates a CSIDriver
+ * object representing the driver. Kubernetes attach detach controller uses this
+ * object to determine whether attach is required. Kubelet uses this object to
+ * determine whether pod information needs to be passed on mount. CSIDriver objects
+ * are non-namespaced.
+ */
+class CSIDriver extends \KubernetesRuntime\AbstractModel
+{
+
+    /**
+     * APIVersion defines the versioned schema of this representation of an object.
+     * Servers should convert recognized schemas to the latest internal value, and may
+     * reject unrecognized values. More info:
+     * https://git.k8s.io/community/contributors/devel/api-conventions.md#resources
+     *
+     * @var string
+     */
+    public $apiVersion = 'storage.k8s.io/v1beta1';
+
+    /**
+     * Kind is a string value representing the REST resource this object represents.
+     * Servers may infer this from the endpoint the client submits requests to. Cannot
+     * be updated. In CamelCase. More info:
+     * https://git.k8s.io/community/contributors/devel/api-conventions.md#types-kinds
+     *
+     * @var string
+     */
+    public $kind = 'CSIDriver';
+
+    /**
+     * Standard object metadata. metadata.Name indicates the name of the CSI driver
+     * that this object refers to; it MUST be the same name returned by the CSI
+     * GetPluginName() call for that driver. The driver name must be 63 characters or
+     * less, beginning and ending with an alphanumeric character ([a-z0-9A-Z]) with
+     * dashes (-), dots (.), and alphanumerics between. More info:
+     * https://git.k8s.io/community/contributors/devel/api-conventions.md#metadata
+     *
+     * @var \Kubernetes\Model\Io\K8s\Apimachinery\Pkg\Apis\Meta\V1\ObjectMeta
+     */
+    public $metadata = null;
+
+    /**
+     * Specification of the CSI Driver.
+     *
+     * @var CSIDriverSpec
+     */
+    public $spec = null;
+
+
+}
+
