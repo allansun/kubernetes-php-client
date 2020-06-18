96dba905736010902748667d2af965d977a8993e
Travis CI User <travis@example.org>
2020-06-18 03:43:14 +0000

    Generated against Kubernetes version v1.16.12-rc.0

---
+++ src/Model/Io/K8s/Api/Node/V1beta1/Overhead.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Kubernetes\Model\Io\K8s\Api\Node\V1beta1;
+
+/**
+ * Overhead structure represents the resource overhead associated with running a
+ * pod.
+ */
+class Overhead extends \KubernetesRuntime\AbstractModel
+{
+
+    /**
+     * PodFixed represents the fixed resource overhead associated with running a pod.
+     *
+     * @var object
+     */
+    public $podFixed = null;
+
+
+}
+
