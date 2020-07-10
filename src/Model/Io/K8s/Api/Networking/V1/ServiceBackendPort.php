eceb09f07e1f717b749c2cc14f1aa9b66569b1be
Travis CI User <travis@example.org>
2020-07-10 03:42:17 +0000

    Generated against Kubernetes version v1.20.0-alpha.0

---
+++ src/Model/Io/K8s/Api/Networking/V1/ServiceBackendPort.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Kubernetes\Model\Io\K8s\Api\Networking\V1;
+
+/**
+ * ServiceBackendPort is the service port being referenced.
+ */
+class ServiceBackendPort extends \KubernetesRuntime\AbstractModel
+{
+
+    /**
+     * Name is the name of the port on the Service. This is a mutually exclusive
+     * setting with "Number".
+     *
+     * @var string
+     */
+    public $name = null;
+
+    /**
+     * Number is the numerical port number (e.g. 80) on the Service. This is a mutually
+     * exclusive setting with "Name".
+     *
+     * @var integer
+     */
+    public $number = null;
+
+
+}
+
