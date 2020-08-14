1431b06c2d61da0a32565f009e9c55dc2ba6c308
Travis CI User <travis@example.org>
2020-08-14 03:42:01 +0000

    Generated against Kubernetes version v1.17.11

---
+++ src/Model/Io/K8s/Kube_aggregator/Pkg/Apis/Apiregistration/V1beta1/ServiceReference.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Kubernetes\Model\Io\K8s\Kube_aggregator\Pkg\Apis\Apiregistration\V1beta1;
+
+/**
+ * ServiceReference holds a reference to Service.legacy.k8s.io
+ */
+class ServiceReference extends \KubernetesRuntime\AbstractModel
+{
+
+    /**
+     * Name is the name of the service
+     *
+     * @var string
+     */
+    public $name = null;
+
+    /**
+     * Namespace is the namespace of the service
+     *
+     * @var string
+     */
+    public $namespace = null;
+
+    /**
+     * If specified, the port on the service that hosting webhook. Default to 443 for
+     * backward compatibility. `port` should be a valid port number (1-65535,
+     * inclusive).
+     *
+     * @var integer
+     */
+    public $port = null;
+
+
+}
+
