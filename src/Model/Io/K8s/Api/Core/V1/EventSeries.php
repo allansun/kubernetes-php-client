be641a2255b99ef915cc1ed7282c399c39b80f5d
Travis CI User <travis@example.org>
2020-08-27 03:41:46 +0000

    Generated against Kubernetes version v1.19.1-rc.0

---
+++ src/Model/Io/K8s/Api/Core/V1/EventSeries.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Kubernetes\Model\Io\K8s\Api\Core\V1;
+
+/**
+ * EventSeries contain information on series of events, i.e. thing that was/is
+ * happening continuously for some time.
+ */
+class EventSeries extends \KubernetesRuntime\AbstractModel
+{
+
+    /**
+     * Number of occurrences in this series up to the last heartbeat time
+     *
+     * @var integer
+     */
+    public $count = null;
+
+    /**
+     * Time of the last occurrence observed
+     *
+     * @var \Kubernetes\Model\Io\K8s\Apimachinery\Pkg\Apis\Meta\V1\MicroTime
+     */
+    public $lastObservedTime = null;
+
+
+}
+
