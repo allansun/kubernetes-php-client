ceb2e979be9067ae9a61015b6273a0ae9031d5c7
Travis CI User <travis@example.org>
2020-08-27 03:42:40 +0000

    Generated against Kubernetes version v1.19.0

---
+++ src/API/ReplicationController.php
@@ -0,0 +1,387 @@
+<?php
+
+namespace Kubernetes\API;
+
+use \Kubernetes\Model\Io\K8s\Api\Core\V1\ReplicationControllerList as ReplicationControllerList;
+use \Kubernetes\Model\Io\K8s\Api\Core\V1\ReplicationController as TheReplicationController;
+use \Kubernetes\Model\Io\K8s\Apimachinery\Pkg\Apis\Meta\V1\DeleteOptions as DeleteOptions;
+use \Kubernetes\Model\Io\K8s\Apimachinery\Pkg\Apis\Meta\V1\Status as Status;
+use \Kubernetes\Model\Io\K8s\Apimachinery\Pkg\Apis\Meta\V1\Patch as Patch;
+use \Kubernetes\Model\Io\K8s\Apimachinery\Pkg\Apis\Meta\V1\WatchEvent as WatchEvent;
+
+class ReplicationController extends \KubernetesRuntime\AbstractAPI
+{
+
+    /**
+     * list or watch objects of kind ReplicationController
+     *
+     * @configkey allowWatchBookmarks	boolean
+     * @configkey continue	string
+     * @configkey fieldSelector	string
+     * @configkey labelSelector	string
+     * @configkey limit	integer
+     * @configkey resourceVersion	string
+     * @configkey resourceVersionMatch	string
+     * @configkey timeoutSeconds	integer
+     * @configkey watch	boolean
+     * @configkey allowWatchBookmarks	boolean
+     * @configkey continue	string
+     * @configkey fieldSelector	string
+     * @configkey labelSelector	string
+     * @configkey limit	integer
+     * @configkey resourceVersion	string
+     * @configkey resourceVersionMatch	string
+     * @configkey timeoutSeconds	integer
+     * @configkey watch	boolean
+     * @param $namespace
+     * @param array $queries
+     * @return ReplicationControllerList|mixed
+     */
+    public function list($namespace = 'default', array $queries = [])
+    {
+        return $this->parseResponse(
+        	$this->client->request('get',
+        		"/api/v1/namespaces/{$namespace}/replicationcontrollers"
+        		,[
+        			'query' => $queries,
+        		]
+        	)
+        	, 'listCoreV1NamespacedReplicationController'
+        );
+    }
+
+    /**
+     * create a ReplicationController
+     *
+     * @configkey dryRun	string
+     * @configkey fieldManager	string
+     * @configkey dryRun	string
+     * @configkey fieldManager	string
+     * @param $namespace
+     * @param TheReplicationController $Model
+     * @param array $queries
+     * @return TheReplicationController|mixed
+     */
+    public function create($namespace = 'default', \Kubernetes\Model\Io\K8s\Api\Core\V1\ReplicationController $Model, array $queries = [])
+    {
+        return $this->parseResponse(
+        	$this->client->request('post',
+        		"/api/v1/namespaces/{$namespace}/replicationcontrollers"
+        		,[
+        			'json' => $Model->getArrayCopy(),
+        			'query' => $queries,
+        		]
+        	)
+        	, 'createCoreV1NamespacedReplicationController'
+        );
+    }
+
+    /**
+     * delete collection of ReplicationController
+     *
+     * @configkey continue	string
+     * @configkey dryRun	string
+     * @configkey fieldSelector	string
+     * @configkey gracePeriodSeconds	integer
+     * @configkey labelSelector	string
+     * @configkey limit	integer
+     * @configkey orphanDependents	boolean
+     * @configkey propagationPolicy	string
+     * @configkey resourceVersion	string
+     * @configkey resourceVersionMatch	string
+     * @configkey timeoutSeconds	integer
+     * @configkey continue	string
+     * @configkey dryRun	string
+     * @configkey fieldSelector	string
+     * @configkey gracePeriodSeconds	integer
+     * @configkey labelSelector	string
+     * @configkey limit	integer
+     * @configkey orphanDependents	boolean
+     * @configkey propagationPolicy	string
+     * @configkey resourceVersion	string
+     * @configkey resourceVersionMatch	string
+     * @configkey timeoutSeconds	integer
+     * @param $namespace
+     * @param DeleteOptions $Model
+     * @param array $queries
+     * @return Status|mixed
+     */
+    public function deleteCollection($namespace = 'default', \Kubernetes\Model\Io\K8s\Apimachinery\Pkg\Apis\Meta\V1\DeleteOptions $Model, array $queries = [])
+    {
+        return $this->parseResponse(
+        	$this->client->request('delete',
+        		"/api/v1/namespaces/{$namespace}/replicationcontrollers"
+        		,[
+        			'json' => $Model->getArrayCopy(),
+        			'query' => $queries,
+        		]
+        	)
+        	, 'deleteCoreV1CollectionNamespacedReplicationController'
+        );
+    }
+
+    /**
+     * read the specified ReplicationController
+     *
+     * @configkey exact	boolean
+     * @configkey export	boolean
+     * @configkey exact	boolean
+     * @configkey export	boolean
+     * @param $namespace
+     * @param $name
+     * @param array $queries
+     * @return TheReplicationController|mixed
+     */
+    public function read($namespace = 'default', $name, array $queries = [])
+    {
+        return $this->parseResponse(
+        	$this->client->request('get',
+        		"/api/v1/namespaces/{$namespace}/replicationcontrollers/{$name}"
+        		,[
+        			'query' => $queries,
+        		]
+        	)
+        	, 'readCoreV1NamespacedReplicationController'
+        );
+    }
+
+    /**
+     * replace the specified ReplicationController
+     *
+     * @configkey dryRun	string
+     * @configkey fieldManager	string
+     * @configkey dryRun	string
+     * @configkey fieldManager	string
+     * @param $namespace
+     * @param $name
+     * @param TheReplicationController $Model
+     * @param array $queries
+     * @return TheReplicationController|mixed
+     */
+    public function replace($namespace = 'default', $name, \Kubernetes\Model\Io\K8s\Api\Core\V1\ReplicationController $Model, array $queries = [])
+    {
+        return $this->parseResponse(
+        	$this->client->request('put',
+        		"/api/v1/namespaces/{$namespace}/replicationcontrollers/{$name}"
+        		,[
+        			'json' => $Model->getArrayCopy(),
+        			'query' => $queries,
+        		]
+        	)
+        	, 'replaceCoreV1NamespacedReplicationController'
+        );
+    }
+
+    /**
+     * delete a ReplicationController
+     *
+     * @configkey dryRun	string
+     * @configkey gracePeriodSeconds	integer
+     * @configkey orphanDependents	boolean
+     * @configkey propagationPolicy	string
+     * @configkey dryRun	string
+     * @configkey gracePeriodSeconds	integer
+     * @configkey orphanDependents	boolean
+     * @configkey propagationPolicy	string
+     * @param $namespace
+     * @param $name
+     * @param DeleteOptions $Model
+     * @param array $queries
+     * @return Status|mixed
+     */
+    public function delete($namespace = 'default', $name, \Kubernetes\Model\Io\K8s\Apimachinery\Pkg\Apis\Meta\V1\DeleteOptions $Model, array $queries = [])
+    {
+        return $this->parseResponse(
+        	$this->client->request('delete',
+        		"/api/v1/namespaces/{$namespace}/replicationcontrollers/{$name}"
+        		,[
+        			'json' => $Model->getArrayCopy(),
+        			'query' => $queries,
+        		]
+        	)
+        	, 'deleteCoreV1NamespacedReplicationController'
+        );
+    }
+
+    /**
+     * partially update the specified ReplicationController
+     *
+     * @configkey dryRun	string
+     * @configkey fieldManager	string
+     * @configkey force	boolean
+     * @configkey dryRun	string
+     * @configkey fieldManager	string
+     * @configkey force	boolean
+     * @param $namespace
+     * @param $name
+     * @param Patch $Model
+     * @param array $queries
+     * @return TheReplicationController|mixed
+     */
+    public function patch($namespace = 'default', $name, \Kubernetes\Model\Io\K8s\Apimachinery\Pkg\Apis\Meta\V1\Patch $Model, array $queries = [])
+    {
+        return $this->parseResponse(
+        	$this->client->request('patch',
+        		"/api/v1/namespaces/{$namespace}/replicationcontrollers/{$name}"
+        		,[
+        			'json' => $Model->getArrayCopy(),
+        			'query' => $queries,
+        		]
+        	)
+        	, 'patchCoreV1NamespacedReplicationController'
+        );
+    }
+
+    /**
+     * read status of the specified ReplicationController
+     *
+     * @param $namespace
+     * @param $name
+     * @return TheReplicationController|mixed
+     */
+    public function readStatus($namespace = 'default', $name)
+    {
+        return $this->parseResponse(
+        	$this->client->request('get',
+        		"/api/v1/namespaces/{$namespace}/replicationcontrollers/{$name}/status"
+        		,[
+        		]
+        	)
+        	, 'readCoreV1NamespacedReplicationControllerStatus'
+        );
+    }
+
+    /**
+     * replace status of the specified ReplicationController
+     *
+     * @configkey dryRun	string
+     * @configkey fieldManager	string
+     * @configkey dryRun	string
+     * @configkey fieldManager	string
+     * @param $namespace
+     * @param $name
+     * @param TheReplicationController $Model
+     * @param array $queries
+     * @return TheReplicationController|mixed
+     */
+    public function replaceStatus($namespace = 'default', $name, \Kubernetes\Model\Io\K8s\Api\Core\V1\ReplicationController $Model, array $queries = [])
+    {
+        return $this->parseResponse(
+        	$this->client->request('put',
+        		"/api/v1/namespaces/{$namespace}/replicationcontrollers/{$name}/status"
+        		,[
+        			'json' => $Model->getArrayCopy(),
+        			'query' => $queries,
+        		]
+        	)
+        	, 'replaceCoreV1NamespacedReplicationControllerStatus'
+        );
+    }
+
+    /**
+     * partially update status of the specified ReplicationController
+     *
+     * @configkey dryRun	string
+     * @configkey fieldManager	string
+     * @configkey force	boolean
+     * @configkey dryRun	string
+     * @configkey fieldManager	string
+     * @configkey force	boolean
+     * @param $namespace
+     * @param $name
+     * @param Patch $Model
+     * @param array $queries
+     * @return TheReplicationController|mixed
+     */
+    public function patchStatus($namespace = 'default', $name, \Kubernetes\Model\Io\K8s\Apimachinery\Pkg\Apis\Meta\V1\Patch $Model, array $queries = [])
+    {
+        return $this->parseResponse(
+        	$this->client->request('patch',
+        		"/api/v1/namespaces/{$namespace}/replicationcontrollers/{$name}/status"
+        		,[
+        			'json' => $Model->getArrayCopy(),
+        			'query' => $queries,
+        		]
+        	)
+        	, 'patchCoreV1NamespacedReplicationControllerStatus'
+        );
+    }
+
+    /**
+     * list or watch objects of kind ReplicationController
+     *
+     * @return ReplicationControllerList|mixed
+     */
+    public function listForAllNamespaces()
+    {
+        return $this->parseResponse(
+        	$this->client->request('get',
+        		"/api/v1/replicationcontrollers"
+        		,[
+        		]
+        	)
+        	, 'listCoreV1ReplicationControllerForAllNamespaces'
+        );
+    }
+
+    /**
+     * watch individual changes to a list of ReplicationController. deprecated: use the
+     * 'watch' parameter with a list operation instead.
+     *
+     * @param $namespace
+     * @return WatchEvent|mixed
+     */
+    public function watchList($namespace = 'default')
+    {
+        return $this->parseResponse(
+        	$this->client->request('get',
+        		"/api/v1/watch/namespaces/{$namespace}/replicationcontrollers"
+        		,[
+        		]
+        	)
+        	, 'watchCoreV1NamespacedReplicationControllerList'
+        );
+    }
+
+    /**
+     * watch changes to an object of kind ReplicationController. deprecated: use the
+     * 'watch' parameter with a list operation instead, filtered to a single item with
+     * the 'fieldSelector' parameter.
+     *
+     * @param $namespace
+     * @param $name
+     * @return WatchEvent|mixed
+     */
+    public function watch($namespace = 'default', $name)
+    {
+        return $this->parseResponse(
+        	$this->client->request('get',
+        		"/api/v1/watch/namespaces/{$namespace}/replicationcontrollers/{$name}"
+        		,[
+        		]
+        	)
+        	, 'watchCoreV1NamespacedReplicationController'
+        );
+    }
+
+    /**
+     * watch individual changes to a list of ReplicationController. deprecated: use the
+     * 'watch' parameter with a list operation instead.
+     *
+     * @return WatchEvent|mixed
+     */
+    public function watchListForAllNamespaces()
+    {
+        return $this->parseResponse(
+        	$this->client->request('get',
+        		"/api/v1/watch/replicationcontrollers"
+        		,[
+        		]
+        	)
+        	, 'watchCoreV1ReplicationControllerListForAllNamespaces'
+        );
+    }
+
+
+}
+
---
+++ src/Model/Io/K8s/Kube_aggregator/Pkg/Apis/Apiregistration/V1/APIServiceCondition.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Kubernetes\Model\Io\K8s\Kube_aggregator\Pkg\Apis\Apiregistration\V1;
+
+/**
+ * APIServiceCondition describes the state of an APIService at a particular point
+ */
+class APIServiceCondition extends \KubernetesRuntime\AbstractModel
+{
+
+    /**
+     * Last time the condition transitioned from one status to another.
+     *
+     * @var \Kubernetes\Model\Io\K8s\Apimachinery\Pkg\Apis\Meta\V1\Time
+     */
+    public $lastTransitionTime = null;
+
+    /**
+     * Human-readable message indicating details about last transition.
+     *
+     * @var string
+     */
+    public $message = null;
+
+    /**
+     * Unique, one-word, CamelCase reason for the condition's last transition.
+     *
+     * @var string
+     */
+    public $reason = null;
+
+    /**
+     * Status is the status of the condition. Can be True, False, Unknown.
+     *
+     * @var string
+     */
+    public $status = null;
+
+    /**
+     * Type is the type of the condition.
+     *
+     * @var string
+     */
+    public $type = null;
+
+
+}
+
