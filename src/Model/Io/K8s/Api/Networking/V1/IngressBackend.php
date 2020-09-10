41140c4fb32c32d9f3238a5460e6deb34177ef4f
Travis CI User <travis@example.org>
2020-09-10 03:41:41 +0000

    Generated against Kubernetes version v1.19.1

---
+++ src/Model/Io/K8s/Api/Networking/V1/IngressBackend.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Kubernetes\Model\Io\K8s\Api\Networking\V1;
+
+/**
+ * IngressBackend describes all endpoints for a given service and port.
+ */
+class IngressBackend extends \KubernetesRuntime\AbstractModel
+{
+
+    /**
+     * Resource is an ObjectRef to another Kubernetes resource in the namespace of the
+     * Ingress object. If resource is specified, a service.Name and service.Port must
+     * not be specified. This is a mutually exclusive setting with "Service".
+     *
+     * @var \Kubernetes\Model\Io\K8s\Api\Core\V1\TypedLocalObjectReference
+     */
+    public $resource = null;
+
+    /**
+     * Service references a Service as a Backend. This is a mutually exclusive setting
+     * with "Resource".
+     *
+     * @var IngressServiceBackend
+     */
+    public $service = null;
+
+
+}
+
