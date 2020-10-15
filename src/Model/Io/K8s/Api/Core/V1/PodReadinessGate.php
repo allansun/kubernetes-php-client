1b15ed4dfd10d482ac056884fa1545bc3aee3ed8
Travis CI User <travis@example.org>
2020-10-15 03:42:01 +0000

    Generated against Kubernetes version v1.19.4-rc.0

---
+++ src/Model/Io/K8s/Api/Core/V1/PodReadinessGate.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Kubernetes\Model\Io\K8s\Api\Core\V1;
+
+use \KubernetesRuntime\AbstractModel;
+
+/**
+ * PodReadinessGate contains the reference to a pod condition
+ */
+class PodReadinessGate extends AbstractModel
+{
+
+    /**
+     * ConditionType refers to a condition in the pod's condition list with matching
+     * type.
+     *
+     * @var string
+     */
+    public $conditionType = null;
+
+
+}
+
---
+++ src/Model/Io/K8s/Api/Core/V1/PodTemplateSpec.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Kubernetes\Model\Io\K8s\Api\Core\V1;
+
+use \KubernetesRuntime\AbstractModel;
+
+/**
+ * PodTemplateSpec describes the data a pod should have when created from a
+ * template
+ */
+class PodTemplateSpec extends AbstractModel
+{
+
+    /**
+     * Standard object's metadata. More info:
+     * https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#metadata
+     *
+     * @var \Kubernetes\Model\Io\K8s\Apimachinery\Pkg\Apis\Meta\V1\ObjectMeta
+     */
+    public $metadata = null;
+
+    /**
+     * Specification of the desired behavior of the pod. More info:
+     * https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#spec-and-status
+     *
+     * @var PodSpec
+     */
+    public $spec = null;
+
+
+}
+
