48904be8b86b1daf21e105c0a8f22867ba297ad8
Travis CI User <travis@example.org>
2020-10-21 03:41:53 +0000

    Generated against Kubernetes version v1.20.0-alpha.3

---
+++ src/Model/Io/K8s/Api/Core/V1/EndpointsList.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Kubernetes\Model\Io\K8s\Api\Core\V1;
+
+use \KubernetesRuntime\AbstractModel;
+
+/**
+ * EndpointsList is a list of endpoints.
+ */
+class EndpointsList extends AbstractModel
+{
+
+    /**
+     * APIVersion defines the versioned schema of this representation of an object.
+     * Servers should convert recognized schemas to the latest internal value, and may
+     * reject unrecognized values. More info:
+     * https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#resources
+     *
+     * @var string
+     */
+    public $apiVersion = 'v1';
+
+    /**
+     * List of endpoints.
+     *
+     * @var Endpoints[]
+     */
+    public $items = null;
+
+    /**
+     * Kind is a string value representing the REST resource this object represents.
+     * Servers may infer this from the endpoint the client submits requests to. Cannot
+     * be updated. In CamelCase. More info:
+     * https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#types-kinds
+     *
+     * @var string
+     */
+    public $kind = 'EndpointsList';
+
+    /**
+     * Standard list metadata. More info:
+     * https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#types-kinds
+     *
+     * @var \Kubernetes\Model\Io\K8s\Apimachinery\Pkg\Apis\Meta\V1\ListMeta
+     */
+    public $metadata = null;
+
+
+}
+
---
+++ src/Model/Io/K8s/Api/Rbac/V1beta1/Role.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Kubernetes\Model\Io\K8s\Api\Rbac\V1beta1;
+
+use \KubernetesRuntime\AbstractModel;
+
+/**
+ * Role is a namespaced, logical grouping of PolicyRules that can be referenced as
+ * a unit by a RoleBinding. Deprecated in v1.17 in favor of
+ * rbac.authorization.k8s.io/v1 Role, and will no longer be served in v1.22.
+ */
+class Role extends AbstractModel
+{
+
+    /**
+     * APIVersion defines the versioned schema of this representation of an object.
+     * Servers should convert recognized schemas to the latest internal value, and may
+     * reject unrecognized values. More info:
+     * https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#resources
+     *
+     * @var string
+     */
+    public $apiVersion = 'rbac.authorization.k8s.io/v1beta1';
+
+    /**
+     * Kind is a string value representing the REST resource this object represents.
+     * Servers may infer this from the endpoint the client submits requests to. Cannot
+     * be updated. In CamelCase. More info:
+     * https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#types-kinds
+     *
+     * @var string
+     */
+    public $kind = 'Role';
+
+    /**
+     * Standard object's metadata.
+     *
+     * @var \Kubernetes\Model\Io\K8s\Apimachinery\Pkg\Apis\Meta\V1\ObjectMeta
+     */
+    public $metadata = null;
+
+    /**
+     * Rules holds all the PolicyRules for this Role
+     *
+     * @var PolicyRule[]
+     */
+    public $rules = null;
+
+
+}
+
