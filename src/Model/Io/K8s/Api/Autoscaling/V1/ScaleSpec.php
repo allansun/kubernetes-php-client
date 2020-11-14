93395f6367fca7501f6eeda3308c39f0f141fdb2
Travis CI User <travis@example.org>
2020-11-14 03:41:46 +0000

    Generated against Kubernetes version v1.18.11

---
+++ src/Model/Io/K8s/Api/Autoscaling/V1/ScaleSpec.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Kubernetes\Model\Io\K8s\Api\Autoscaling\V1;
+
+use \KubernetesRuntime\AbstractModel;
+
+/**
+ * ScaleSpec describes the attributes of a scale subresource.
+ */
+class ScaleSpec extends AbstractModel
+{
+
+    /**
+     * desired number of instances for the scaled object.
+     *
+     * @var integer
+     */
+    public $replicas = null;
+
+
+}
+
