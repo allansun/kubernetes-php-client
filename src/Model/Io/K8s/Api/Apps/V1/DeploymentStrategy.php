16547b8c7c9e91fd03c6ea0ebbb724501d0c14fc
Travis CI User <travis@example.org>
2020-12-15 21:41:15 +0000

    Generated against Kubernetes version v1.19.6-rc.0

---
+++ src/Model/Io/K8s/Api/Apps/V1/DeploymentStrategy.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Kubernetes\Model\Io\K8s\Api\Apps\V1;
+
+use \KubernetesRuntime\AbstractModel;
+
+/**
+ * DeploymentStrategy describes how to replace existing pods with new ones.
+ */
+class DeploymentStrategy extends AbstractModel
+{
+
+    /**
+     * Rolling update config params. Present only if DeploymentStrategyType =
+     * RollingUpdate.
+     *
+     * @var RollingUpdateDeployment
+     */
+    public $rollingUpdate = null;
+
+    /**
+     * Type of deployment. Can be "Recreate" or "RollingUpdate". Default is
+     * RollingUpdate.
+     *
+     * @var string
+     */
+    public $type = null;
+
+
+}
+
---
+++ src/Model/Io/K8s/Api/Core/V1/PreferredSchedulingTerm.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Kubernetes\Model\Io\K8s\Api\Core\V1;
+
+use \KubernetesRuntime\AbstractModel;
+
+/**
+ * An empty preferred scheduling term matches all objects with implicit weight 0
+ * (i.e. it's a no-op). A null preferred scheduling term matches no objects (i.e.
+ * is also a no-op).
+ */
+class PreferredSchedulingTerm extends AbstractModel
+{
+
+    /**
+     * A node selector term, associated with the corresponding weight.
+     *
+     * @var NodeSelectorTerm
+     */
+    public $preference = null;
+
+    /**
+     * Weight associated with matching the corresponding nodeSelectorTerm, in the range
+     * 1-100.
+     *
+     * @var integer
+     */
+    public $weight = null;
+
+
+}
+
