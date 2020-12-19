73a0495f5a08ef90dc777cfa71a69272f8087f68
Travis CI User <travis@example.org>
2020-12-19 03:41:40 +0000

    Generated against Kubernetes version v1.20.2-rc.0

---
+++ src/Model/Io/K8s/Apiextensions_apiserver/Pkg/Apis/Apiextensions/V1beta1/JSONSchemaPropsOrArray.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Kubernetes\Model\Io\K8s\Apiextensions_apiserver\Pkg\Apis\Apiextensions\V1beta1;
+
+use \KubernetesRuntime\AbstractModel;
+
+/**
+ * JSONSchemaPropsOrArray represents a value that can either be a JSONSchemaProps
+ * or an array of JSONSchemaProps. Mainly here for serialization purposes.
+ */
+class JSONSchemaPropsOrArray extends AbstractModel
+{
+
+
+}
+
