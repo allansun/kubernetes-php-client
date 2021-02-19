8896cd093e756bf7a24361f2da7149571e04297f
Travis CI User <travis@example.org>
2021-02-19 04:25:51 +0000

    Generated against Kubernetes version v1.20.4-rc.0

---
+++ src/Model/Io/K8s/Api/Authentication/V1/TokenRequest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Kubernetes\Model\Io\K8s\Api\Authentication\V1;
+
+use \KubernetesRuntime\AbstractModel;
+
+/**
+ * TokenRequest requests a token for a given service account.
+ */
+class TokenRequest extends AbstractModel
+{
+
+    /**
+     * APIVersion defines the versioned schema of this representation of an object.
+     * Servers should convert recognized schemas to the latest internal value, and may
+     * reject unrecognized values. More info:
+     * https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#resources
+     *
+     * @var string
+     */
+    public $apiVersion = 'authentication.k8s.io/v1';
+
+    /**
+     * Kind is a string value representing the REST resource this object represents.
+     * Servers may infer this from the endpoint the client submits requests to. Cannot
+     * be updated. In CamelCase. More info:
+     * https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#types-kinds
+     *
+     * @var string
+     */
+    public $kind = 'TokenRequest';
+
+    /**
+     * @var \Kubernetes\Model\Io\K8s\Apimachinery\Pkg\Apis\Meta\V1\ObjectMeta
+     */
+    public $metadata = null;
+
+    /**
+     * @var TokenRequestSpec
+     */
+    public $spec = null;
+
+    /**
+     * @var TokenRequestStatus
+     */
+    public $status = null;
+
+
+}
+
---
+++ src/Model/Io/K8s/Api/Flowcontrol/V1beta1/ServiceAccountSubject.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Kubernetes\Model\Io\K8s\Api\Flowcontrol\V1beta1;
+
+use \KubernetesRuntime\AbstractModel;
+
+/**
+ * ServiceAccountSubject holds detailed information for service-account-kind
+ * subject.
+ */
+class ServiceAccountSubject extends AbstractModel
+{
+
+    /**
+     * `name` is the name of matching ServiceAccount objects, or "*" to match
+     * regardless of name. Required.
+     *
+     * @var string
+     */
+    public $name = null;
+
+    /**
+     * `namespace` is the namespace of matching ServiceAccount objects. Required.
+     *
+     * @var string
+     */
+    public $namespace = null;
+
+
+}
+
