87b814ec50d55a8871bbbc38debe2a82a53f94b4
Travis CI User <travis@example.org>
2021-02-20 03:42:30 +0000

    Generated against Kubernetes version v1.19.9-rc.0

---
+++ src/Model/Io/K8s/Api/Autoscaling/V2beta2/PodsMetricSource.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Kubernetes\Model\Io\K8s\Api\Autoscaling\V2beta2;
+
+use \KubernetesRuntime\AbstractModel;
+
+/**
+ * PodsMetricSource indicates how to scale on a metric describing each pod in the
+ * current scale target (for example, transactions-processed-per-second). The
+ * values will be averaged together before being compared to the target value.
+ */
+class PodsMetricSource extends AbstractModel
+{
+
+    /**
+     * metric identifies the target metric by name and selector
+     *
+     * @var MetricIdentifier
+     */
+    public $metric = null;
+
+    /**
+     * target specifies the target value for the given metric
+     *
+     * @var MetricTarget
+     */
+    public $target = null;
+
+
+}
+
---
+++ src/Model/Io/K8s/Api/Core/V1/ResourceQuotaStatus.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Kubernetes\Model\Io\K8s\Api\Core\V1;
+
+use \KubernetesRuntime\AbstractModel;
+
+/**
+ * ResourceQuotaStatus defines the enforced hard limits and observed use.
+ */
+class ResourceQuotaStatus extends AbstractModel
+{
+
+    /**
+     * Hard is the set of enforced hard limits for each named resource. More info:
+     * https://kubernetes.io/docs/concepts/policy/resource-quotas/
+     *
+     * @var object
+     */
+    public $hard = null;
+
+    /**
+     * Used is the current observed total usage of the resource in the namespace.
+     *
+     * @var object
+     */
+    public $used = null;
+
+
+}
+
