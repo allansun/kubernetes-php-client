0fc54b3e2529b4082efa298dbc5a49ae8b394b65
Travis CI User <travis@example.org>
2021-04-17 03:41:49 +0000

    Generated against Kubernetes version v1.19.11-rc.0

---
+++ src/Model/Io/K8s/Apiextensions_apiserver/Pkg/Apis/Apiextensions/V1beta1/CustomResourceValidation.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Kubernetes\Model\Io\K8s\Apiextensions_apiserver\Pkg\Apis\Apiextensions\V1beta1;
+
+use \KubernetesRuntime\AbstractModel;
+
+/**
+ * CustomResourceValidation is a list of validation methods for CustomResources.
+ */
+class CustomResourceValidation extends AbstractModel
+{
+
+    /**
+     * openAPIV3Schema is the OpenAPI v3 schema to use for validation and pruning.
+     *
+     * @var JSONSchemaProps
+     */
+    public $openAPIV3Schema = null;
+
+
+}
+
