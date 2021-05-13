5fc98b75e64528bf060ab4bf7a8a486f6271b4e5
Travis CI User <travis@example.org>
2021-05-13 07:40:48 +0000

    Generated against Kubernetes version v1.18.20-rc.0

---
+++ src/Model/Io/K8s/Api/Core/V1/VolumeNodeAffinity.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Kubernetes\Model\Io\K8s\Api\Core\V1;
+
+use \KubernetesRuntime\AbstractModel;
+
+/**
+ * VolumeNodeAffinity defines constraints that limit what nodes this volume can be
+ * accessed from.
+ */
+class VolumeNodeAffinity extends AbstractModel
+{
+
+    /**
+     * Required specifies hard node constraints that must be met.
+     *
+     * @var NodeSelector
+     */
+    public $required = null;
+
+
+}
+
