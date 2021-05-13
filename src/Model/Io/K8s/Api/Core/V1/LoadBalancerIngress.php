a1f307d41baa13a1f1c35d01ce699cfc62051158
Travis CI User <travis@example.org>
2021-05-13 03:41:43 +0000

    Generated against Kubernetes version v1.20.8-rc.0

---
+++ src/Model/Io/K8s/Api/Core/V1/LoadBalancerIngress.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Kubernetes\Model\Io\K8s\Api\Core\V1;
+
+use \KubernetesRuntime\AbstractModel;
+
+/**
+ * LoadBalancerIngress represents the status of a load-balancer ingress point:
+ * traffic intended for the service should be sent to an ingress point.
+ */
+class LoadBalancerIngress extends AbstractModel
+{
+
+    /**
+     * Hostname is set for load-balancer ingress points that are DNS based (typically
+     * AWS load-balancers)
+     *
+     * @var string
+     */
+    public $hostname = null;
+
+    /**
+     * IP is set for load-balancer ingress points that are IP based (typically GCE or
+     * OpenStack load-balancers)
+     *
+     * @var string
+     */
+    public $ip = null;
+
+    /**
+     * Ports is a list of records of service ports If used, every port defined in the
+     * service should have an entry in it
+     *
+     * @var PortStatus[]
+     */
+    public $ports = null;
+
+
+}
+
---
+++ src/Model/Io/K8s/Apiextensions_apiserver/Pkg/Apis/Apiextensions/V1beta1/ExternalDocumentation.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Kubernetes\Model\Io\K8s\Apiextensions_apiserver\Pkg\Apis\Apiextensions\V1beta1;
+
+use \KubernetesRuntime\AbstractModel;
+
+/**
+ * ExternalDocumentation allows referencing an external resource for extended
+ * documentation.
+ */
+class ExternalDocumentation extends AbstractModel
+{
+
+    /**
+     * @var string
+     */
+    public $description = null;
+
+    /**
+     * @var string
+     */
+    public $url = null;
+
+
+}
+
