888cce720d692d7fa84cf589fa93d33c2fd2fd0e
Travis CI User <travis@example.org>
2021-06-21 08:23:49 +0000

    Generated against Kubernetes version v1.19.13-rc.0

---
+++ src/Model/Io/K8s/Api/Networking/V1beta1/IngressClass.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Kubernetes\Model\Io\K8s\Api\Networking\V1beta1;
+
+use \KubernetesRuntime\AbstractModel;
+
+/**
+ * IngressClass represents the class of the Ingress, referenced by the Ingress
+ * Spec. The `ingressclass.kubernetes.io/is-default-class` annotation can be used
+ * to indicate that an IngressClass should be considered default. When a single
+ * IngressClass resource has this annotation set to true, new Ingress resources
+ * without a class specified will be assigned this default class.
+ */
+class IngressClass extends AbstractModel
+{
+
+    /**
+     * APIVersion defines the versioned schema of this representation of an object.
+     * Servers should convert recognized schemas to the latest internal value, and may
+     * reject unrecognized values. More info:
+     * https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#resources
+     *
+     * @var string
+     */
+    public $apiVersion = 'networking.k8s.io/v1beta1';
+
+    /**
+     * Kind is a string value representing the REST resource this object represents.
+     * Servers may infer this from the endpoint the client submits requests to. Cannot
+     * be updated. In CamelCase. More info:
+     * https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#types-kinds
+     *
+     * @var string
+     */
+    public $kind = 'IngressClass';
+
+    /**
+     * Standard object's metadata. More info:
+     * https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#metadata
+     *
+     * @var \Kubernetes\Model\Io\K8s\Apimachinery\Pkg\Apis\Meta\V1\ObjectMeta
+     */
+    public $metadata = null;
+
+    /**
+     * Spec is the desired state of the IngressClass. More info:
+     * https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#spec-and-status
+     *
+     * @var IngressClassSpec
+     */
+    public $spec = null;
+
+
+}
+
---
+++ src/Model/Io/K8s/Apiextensions_apiserver/Pkg/Apis/Apiextensions/V1/CustomResourceDefinitionNames.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Kubernetes\Model\Io\K8s\Apiextensions_apiserver\Pkg\Apis\Apiextensions\V1;
+
+use \KubernetesRuntime\AbstractModel;
+
+/**
+ * CustomResourceDefinitionNames indicates the names to serve this
+ * CustomResourceDefinition
+ */
+class CustomResourceDefinitionNames extends AbstractModel
+{
+
+    /**
+     * categories is a list of grouped resources this custom resource belongs to (e.g.
+     * 'all'). This is published in API discovery documents, and used by clients to
+     * support invocations like `kubectl get all`.
+     *
+     * @var string[]
+     */
+    public $categories = null;
+
+    /**
+     * kind is the serialized kind of the resource. It is normally CamelCase and
+     * singular. Custom resource instances will use this value as the `kind` attribute
+     * in API calls.
+     *
+     * @var string
+     */
+    public $kind = null;
+
+    /**
+     * listKind is the serialized kind of the list for this resource. Defaults to
+     * "`kind`List".
+     *
+     * @var string
+     */
+    public $listKind = null;
+
+    /**
+     * plural is the plural name of the resource to serve. The custom resources are
+     * served under `/apis/<group>/<version>/.../<plural>`. Must match the name of the
+     * CustomResourceDefinition (in the form `<names.plural>.<group>`). Must be all
+     * lowercase.
+     *
+     * @var string
+     */
+    public $plural = null;
+
+    /**
+     * shortNames are short names for the resource, exposed in API discovery documents,
+     * and used by clients to support invocations like `kubectl get <shortname>`. It
+     * must be all lowercase.
+     *
+     * @var string[]
+     */
+    public $shortNames = null;
+
+    /**
+     * singular is the singular name of the resource. It must be all lowercase.
+     * Defaults to lowercased `kind`.
+     *
+     * @var string
+     */
+    public $singular = null;
+
+
+}
+
